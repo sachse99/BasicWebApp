c33a4d2506badf64d3b763685afae540b29c4723
tilsachse <til.sachse@googlemail.com>
2022-07-13 10:50:48 +0200

    x

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class QueryProcessor {
-// Comment //
+// Comment //p
     public String process(String query) {
 		query = query.toLowerCase();
         if (query.contains("shakespeare")) {
