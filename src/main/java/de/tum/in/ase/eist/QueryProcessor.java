56095e84bc0682a4ad607cdcec42456f9664c1f4
tilsachse <til.sachse@googlemail.com>
2022-07-13 11:26:40 +0200

    x

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,19 +2,33 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class QueryProcessor {
-// Comment //psd
+    // Comment //psd
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Til";
-        } else { // TODO extend the programm here
-            return "";
+            return "Til";
+        } else if (query.contains("plus")) {
+            String[] params = query.split(" ");
+            int sum = 0;
+            for (String s : params) {
+                try {
+                    int n = Integer.parseInt(s);
+                    sum += n;
+
+                } catch (NumberFormatException e) {
+                    //
+                }
+            }
+            return String.valueOf(sum);
         }
+        return "";
     }
 }
